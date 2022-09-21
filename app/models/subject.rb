c3e64290140b748544b38bb8ddde8590e8681141
meet <meetmakwana203@gmail.com>
2022-09-21 17:33:16 +0530

    V39

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,12 @@
 class Subject < ApplicationRecord
+
+    # making a scope with name "visible"
+    scope :visible, lambda {where(:visible => true)}
+    scope :invisible, lambda {where(:visible => false)}
+    scope :sorted, lambda { order("position ASC") }
+    scope :newest_first, lambda { order("created_at DESC") }
+
+    # scope with an arguement "query", using array syntax 
+    # 2nd value in array condition will have % sign to indicate SQL syntax and then the query will be safely placed in the place of ? as it is a dynamic data
+    scope :search, lambda { |query| where("name LIKE ?", "%#{query}%")}
 end
