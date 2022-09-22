fd508a9e89d6f2bbc32a6968bbf75ecbf84c0c88
meet <meetmakwana203@gmail.com>
2022-09-22 20:49:52 +0530

    V43

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,6 @@
 class Page < ApplicationRecord
         
-    # completing one-to-one associations
-    belongs_to :subject 
+    # completing one-to-many associations
+    # optional is false by default 
+    belongs_to :subject, {:optional => true} 
 end
