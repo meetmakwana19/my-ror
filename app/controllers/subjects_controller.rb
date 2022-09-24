e40af361556924c3ddde6ed0f3d893d181c5762f
meet <meetmakwana203@gmail.com>
2022-09-24 21:42:25 +0530

    V51

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,12 @@
 class SubjectsController < ApplicationController
   def index
+    # FInd subjects and assign them to instance variable so that the view template can access them
+    # @subjects = Subject.all
+    # will return all the subjects as an array and store it in the instance variable @subjects
+    @subjects = Subject.sorted #using the scope created in the model subject.rb
+
+    # no need to tell rails to render this template as it will do it automatically by looking at the method name 
+    # render('index')
   end
 
   def create
