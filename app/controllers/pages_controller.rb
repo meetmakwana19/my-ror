8567e8f8fcc5d10cf6e2cf508d02c5d12df18496
meet <meetmakwana203@gmail.com>
2022-10-07 22:37:36 +0530

    V85

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,14 @@ class PagesController < ApplicationController
 
   layout "admin"
 
+  # by default this controller filter will be applied to methods but putting an only option.
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  # so before these methods, the find_subjects instance variable be set for all those methods.
+
+  before_action :set_page_coount, :only => [:new, :create, :edit, :update]
+
   def index
-    # @pages = Page.all
+    #  @pages = Page.all
     @pages = Page.sorted
   end
 
@@ -13,8 +19,8 @@ class PagesController < ApplicationController
 
   def new
     @page = Page.new
-    @page_count = Page.count + 1
-    @subjects = Subject.sorted
+    # @page_count = Page.count + 1
+    # @subjects = Subject.sorted
   end
   
   def create
@@ -23,16 +29,16 @@ class PagesController < ApplicationController
       flash[:notice] = "Page created succcessfully"
       redirect_to(pages_path)
     else
-      @page_count = Page.count + 1
-      @subjects = Subject.sorted
+      # @page_count = Page.count + 1
+      # @subjects = Subject.sorted
       render("new")
     end
   end
 
   def edit
     @page = Page.find(params[:id])
-    @page_count = Page.count 
-    @subjects = Subject.sorted
+    # @page_count = Page.count 
+    # @subjects = Subject.sorted
   end
   
   def update
@@ -41,8 +47,8 @@ class PagesController < ApplicationController
       flash[:notice] = "Page updated succcessfully"
       redirect_to(pages_path(@page))
     else
-      @page_count = Page.count 
-      @subjects = Subject.sorted
+      # @page_count = Page.count 
+      # @subjects = Subject.sorted
       render("edit")
     end
   end
@@ -63,4 +69,15 @@ class PagesController < ApplicationController
     params.require(:page).permit(:name, :subject_id, :permalink, :position, :visible)
   end
 
+  def find_subjects
+    @subjects = Subject.sorted
+  end
+
+  def set_page_coount 
+    @page_count = Page.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @page_count += 1
+    end
+  end
+  
 end
